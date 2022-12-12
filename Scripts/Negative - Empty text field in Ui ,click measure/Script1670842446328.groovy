e5b5c5c8a90de51e1ebf70d49133d3c34a41e9de
Bharath <bharath@nsqrd.com>
2022-12-12 16:49:48 +0530

    Empty field ,click measure

---
+++ Scripts/Negative - Empty text field in Ui ,click measure/Script1670842446328.groovy
@@ -0,0 +1,68 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\Linus\\Downloads\\Quent_V2022.22 V1.1.51_101120220834_staging.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/Negative - Empty text field ,click measure/android.widget.CheckBox - User Trial'), 
+    0)
+
+Mobile.switchToNative()
+
+Mobile.tap(findTestObject('Object Repository/Negative - Empty text field ,click measure/android.widget.CheckBox'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Negative - Empty text field ,click measure/android.widget.Button - Next'), 
+    0)
+
+Mobile.switchToNative()
+
+Mobile.tap(findTestObject('Object Repository/Negative - Empty text field ,click measure/android.widget.ImageButton'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Negative - Empty text field ,click measure/android.widget.ImageView (1)'), 
+    0)
+
+Mobile.sendKeys(findTestObject('Object Repository/Negative - Empty text field ,click measure/android.widget.EditText - Phone number'), 
+    '8050694262')
+
+Mobile.sendKeys(findTestObject('Object Repository/Negative - Empty text field ,click measure/android.widget.EditText - Password'), 
+    'Test@123')
+
+Mobile.tap(findTestObject('Object Repository/Negative - Empty text field ,click measure/android.widget.Button - Sign In'), 
+    0)
+
+Mobile.switchToNative()
+
+Mobile.scrollToText('Measure')
+
+Mobile.tap(findTestObject('Object Repository/Negative - Empty text field ,click measure/android.widget.Button - Measure'), 
+    0)
+
+Mobile.scrollToText('User info')
+
+Mobile.tap(findTestObject('Object Repository/Negative - Empty text field ,click measure/android.widget.ImageView (2)'), 
+    0)
+
+Mobile.tap(findTestObject('Object Repository/Negative - Empty text field ,click measure/android.widget.TextView - Signout'), 
+    0)
+
+Mobile.tap(findTestObject('Object Repository/Negative - Empty text field ,click measure/android.widget.Button - YES'), 0)
+
+Mobile.switchToNative()
+
+Mobile.closeApplication()
+
---
+++ Test Suites/Empty text field in Ui.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Empty text field in Ui</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7634b83c-085c-494a-801f-cf07c573caa1</testSuiteGuid>
+   <testCaseLink>
+      <guid>489cea45-de96-43eb-95b0-94e4fc15cda0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Negative - Empty text field in Ui ,click measure</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
