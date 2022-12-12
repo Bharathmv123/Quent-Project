17f291d02835cae0831a39774d33a7e892387a43
Bharath <bharath@nsqrd.com>
2022-12-12 19:11:24 +0530

    Pairing

---
+++ Test Suites/Incomplete filling in user info.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Incomplete filling in user info</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7c813e57-4b7b-4f51-a84e-0e7dfb0a98b0</testSuiteGuid>
+   <testCaseLink>
+      <guid>02bb211a-5a16-4ff3-85e0-b6ccb17c5cf1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Neg-Incomplete filling in user info</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
