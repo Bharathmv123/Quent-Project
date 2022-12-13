30523650a86bbc756c0213fe483fd54f64d5eeca
Bharath <bharath@nsqrd.com>
2022-12-13 16:20:05 +0530

    deleted positive test cases

--- Test Suites/En.ts
+++ Test Suites/En.ts
@@ -11,11 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>9d16615d-eb44-4631-b311-5e1e5d7502a0</testSuiteGuid>
-   <testCaseLink>
-      <guid>38fc5777-49ae-47ae-a6b0-73c840356154</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/E - Appium</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/End to End.ts
+++ Test Suites/End to End.ts
@@ -11,11 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>02ed2c73-5cbf-4b41-83df-74ed81033d43</testSuiteGuid>
-   <testCaseLink>
-      <guid>a6273034-ef54-4730-9d32-991c63e9b8d1</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/End to End</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
 </TestSuiteEntity>
