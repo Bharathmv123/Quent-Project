0fba0c31e3dd4b6f8dedf3a2a620db0a87b519af
Bharath <bharath@nsqrd.com>
2022-12-28 17:48:35 +0530

    User flow 3 test cases added

---
+++ Scripts/N/Script1672228086039.groovy
@@ -0,0 +1,41 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\Linus\\Downloads\\Quent_V2022.24 V1.1.55_131220220959_staging.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/N/android.widget.ImageView'), 0)
+
+Mobile.switchToNative()
+
+Mobile.tap(findTestObject('Object Repository/N/android.widget.ImageView (1)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/N/androidx.appcompat.widget.LinearLayoutCompat'), 0)
+
+Mobile.switchToNative()
+
+Mobile.scrollToText('Blood pressure')
+
+Mobile.tap(findTestObject('Object Repository/N/android.widget.ImageView (3)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/N/android.widget.TextView - Measure'), 0)
+
+Mobile.switchToNative()
+
+Mobile.closeApplication()
+
