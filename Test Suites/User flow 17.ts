f34a473436d3b145c6a1e9a2d4084b9ab2984c60
Bharath <bharath@nsqrd.com>
2023-01-12 10:39:45 +0530

    user flow 19 added

---
+++ Test Suites/User flow 17.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>User flow 17</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ab2407ab-b418-4180-b13e-5e85ea4a96d5</testSuiteGuid>
+   <testCaseLink>
+      <guid>b9a69681-214b-4230-ba12-688e0540d680</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User flow 17 - As a user want to login into the app and add user details and ondemand vitals ( HR , ECG) - 1.1.57</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
