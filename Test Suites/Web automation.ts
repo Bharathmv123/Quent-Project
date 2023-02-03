5dcbe0dc6de8bcf5555fdc5e468de7356b94812b
Bharath <bharath@nsqrd.com>
2023-02-03 12:42:44 +0530

    test suite web altered added

--- Test Suites/Web automation.ts
+++ Test Suites/Web automation.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Web - Automation</name>
+   <name>Web automation</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,5 +10,12 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>daba1937-253b-4397-988e-aa9a11ad33e2</testSuiteGuid>
+   <testSuiteGuid>9a16a715-26f5-48d6-93b9-60849b88a5d7</testSuiteGuid>
+   <testCaseLink>
+      <guid>8e42008f-6276-40a3-9af7-8876ed5e3dc3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web CE - Slider</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
