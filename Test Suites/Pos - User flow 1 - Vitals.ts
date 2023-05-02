80b36bea095bcc372269b756bc80aa335bfd440a
Bharath <bharath@nsqrd.com>
2023-05-02 23:56:18 +0530

    Hr SpO2 added

---
+++ Test Suites/Pos - User flow 1 - Vitals.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Pos - User flow 1 - Vitals</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>100e6396-675b-4ad8-b174-cfc0b40c9507</testSuiteGuid>
+   <testCaseLink>
+      <guid>25b36f16-57ff-4ad9-912a-eb1d44f912e2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Pos User flow 1 - HR , SpO2 - 1.70</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
