e69fe056304b53b53ae92611f490be1ee8838afd
Bharath <bharath@nsqrd.com>
2023-05-08 17:22:09 +0530

    new tc added

--- Test Suites/Web test case.ts
+++ Test Suites/Web test case.ts
@@ -12,28 +12,28 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>0bc3cb26-78d3-4c3e-88ad-8071e163e3d7</testSuiteGuid>
    <testCaseLink>
-      <guid>48603934-790e-424f-b7b1-1858326c5c17</guid>
+      <guid>6532f841-650c-477e-a2ba-1a954fc548f8</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Web PUT- Login to BP</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>30bcd84a-76d6-4389-87fc-482c87d536d5</guid>
+      <guid>159c7e97-5452-4322-87c9-a2442cc8ca1d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Web PUT- Login to CBT</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>bc811440-7ce9-4921-8c2c-8ef85c043bd8</guid>
+      <guid>f7ccd51a-199c-4858-bdbb-e5912f8654e4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Web PUT- Login to HR</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>8a3c571d-1801-4866-95dd-020a50d444f8</guid>
+      <guid>f370d61a-135b-4d4d-9982-624cfcb5d558</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Web - Login to SpO2</testCaseId>
