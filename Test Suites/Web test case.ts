d2219fc4ac65ceb07358a93dabf5ba9dad256804
Aravindan <aravindan@divum.in>
2023-06-14 12:21:08 +0530

    web-test-cases

--- Test Suites/Web test case.ts
+++ Test Suites/Web test case.ts
@@ -12,10 +12,31 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>0bc3cb26-78d3-4c3e-88ad-8071e163e3d7</testSuiteGuid>
    <testCaseLink>
-      <guid>08506fa5-d620-492c-8db6-908f73c4cf1b</guid>
+      <guid>92f81bcf-3af7-466c-874f-cdf467eff355</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Web - Login to No of pages , navigation of page no</testCaseId>
+      <testCaseId>Test Cases/Web PUT- Login to HR</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>639661a0-4cc2-482f-94ac-a9620f26ede4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web - Login to SpO2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>730ec91a-8113-4314-bf51-57bd9af8a4d3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web PUT- Login to CBT</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d655fdc3-76ae-4534-9b1b-81d461248667</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web PUT- Login to BP</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
