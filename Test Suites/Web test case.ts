c3964cb4e871f7cd6cee6cd1b464beb983bf1a45
Bharath <bharath@nsqrd.com>
2023-06-14 13:01:17 +0530

    tc added new one

--- Test Suites/Web test case.ts
+++ Test Suites/Web test case.ts
@@ -12,17 +12,17 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>0bc3cb26-78d3-4c3e-88ad-8071e163e3d7</testSuiteGuid>
    <testCaseLink>
-      <guid>af045467-1360-4510-9436-05fa22461df0</guid>
+      <guid>7f1e0ce8-299d-4630-85ae-117f72df8629</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Web - Login to No of pages , navigation of page no</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>fb6de5fe-0190-4c68-bdb6-9ba16a8da9f1</guid>
+      <guid>0de658c7-cd62-4493-92bf-5f4bbb355739</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/WEB PUT - OTA</testCaseId>
+      <testCaseId>Test Cases/Web - PUT - New test case</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
